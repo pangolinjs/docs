99d9908f83ea9e89c131c6abc7222faa04c0282b
Fynn Becker <post@fynn.be>
2019-10-13 09:27:17 +0200

    Add Netlify badge

---
+++ .vuepress/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <a href="https://www.netlify.com">
+      <img
+        alt="Deploys by Netlify"
+        src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"
+      >
+    </a>
+    <p>Licensed under MPL-2.0 and MIT | Copyright © Fynn Becker</p>
+  </footer>
+</template>
